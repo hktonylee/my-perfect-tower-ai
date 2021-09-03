889e8746db5dfeb76b254577a15704ebf697cbe6
Tony Lee <iamtonylee@gmail.com>
2021-09-02 22:35:35 -0700

    Added more scripts

--- meta_scripts/generate_gotoSwitch.py
+++ meta_scripts/generate_gotoSwitch.py
@@ -1,14 +1,17 @@
 from lib import *
 
 def generate(n):
-    s1 = seq2(lambda i: f'cond{i}, cond{i}', n, join=', \\\n    ')
+    s1 = seq2(lambda i: f'cond{i}, label{i}', n, join=', \\\n    ')
     s2 = seq2(lambda i: f'({{cond{i}}}), ({{label{i}}})', n, 2, join=', \\\n    ')
+    s3 = seq2(lambda i: f'({{cond{i}}})', n, join=' || ')
     return f'''
-#gotoSwitch.{n}(default, \\
+#gotoSwitch.{n}( \\
     {s1} \\
-) goto(switch.{n}(int, ({{default}}), \\
+) goto({{switch.{n}(int, ({{label1}}), \\
     {s2} \\
-))
+)}}, \\
+    {s3} \\
+)
 '''
 
 #double.min.6(a, b, c, d, e, f) double.min(({a}), double.min(({b}), double.min(({c}), double.min(({d}), double.min(({e}), ({f}))))))
